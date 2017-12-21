1594059e543fbab15c3a786e6fa807a36a97befc
pablos <ppapad_33@yahoo.gr>
2017-12-21 15:42:39 +0200

    html response added

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,5 +2,10 @@ self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+	event.respondWith(
+		new Response("<p class='a-winner-is-me'>this is a paragraph</p>", {
+			headers: {"Content-Type":"text/html"}
+		})
+	);
+  console.log(event.request); 
 });
