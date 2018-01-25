c9d11fc6a0536b605b7a17bbb2635ae2b7d83900
pablos <ppapad_33@yahoo.gr>
2018-01-25 13:46:17 +0200

    add favotite animal in keyval store

--- public/js/idb-test/index.js
+++ public/js/idb-test/index.js
@@ -28,4 +28,8 @@ dbPromise.then(function(db) {
   // TODO: in the keyval store, set
   // "favoriteAnimal" to your favourite animal
   // eg "cat" or "dog"
+  var tx = db.transaction('keyval', 'readwrite');
+  var keyValStore = tx.objectStore('keyval');
+  keyValStore.put('cat','favoriteAnimal');
+  return tx.complete;
 });
